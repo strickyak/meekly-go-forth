c3b7d968bfacb58d5140ec35b06cb6759dad9995
strick <strick@yak.net>
2020-09-27 01:12:46 -0700

    more for forth-homework-2020-09-26, but hit limitations.

--- forth.go
+++ forth.go
@@ -1,11 +1,18 @@
 package forth
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
+	"runtime/debug"
+	"sort"
 	"strconv"
 	"strings"
 )
+
+var V = flag.Bool("v", false, "verbosity")
+var E = flag.Bool("e", false, "show error stack")
 
 type (
 	Dict  map[string]func()
@@ -18,13 +25,22 @@ type (
 		Immediate Idict
 		D         Stack
 		R         Stack
-		input     []string
-		Mem       [100]int
+		Input     string
+		Mem       [100000]int
 		Here      int
 		Emit      EmitFn
+		RecentDef string
 	}
-	EmitFn func(ch byte)
+	EmitFn                func(ch byte)
+	CompileThisWordSignal string
 )
+
+func Panicf(format string, args ...interface{}) {
+	log.Printf("==========================================")
+	debug.PrintStack()
+	log.Printf("==========================================")
+	log.Panicf(format, args...)
+}
 
 func RunForth(prog string) string {
 	var output []byte
@@ -57,10 +73,43 @@ func (o *Stack) String() string {
 	return fmt.Sprintf("%#v", o.v)
 }
 
-func (f *Forth) nextWord() string {
-	word := f.input[0]
-	f.input = f.input[1:]
-	return word
+func (f *Forth) NextChar() (rune, bool) {
+	if len(f.Input) == 0 {
+		return 0, false
+	}
+	// TODO strings.NewReader. but for now, bytes.
+	var z rune = rune(f.Input[0])
+	f.Input = f.Input[1:]
+	return z, true
+}
+func (f *Forth) NextWord() (string, bool) {
+	var r rune
+	var ok bool
+	// Consume white space.  Leave first non-white in r.
+	for {
+		r, ok = f.NextChar()
+		if !ok {
+			return "", false
+		}
+		if r > 32 {
+			break
+		}
+		// consume all control chars as white space
+	}
+
+	var buf strings.Builder
+	buf.WriteRune(r)
+	for {
+		r, ok = f.NextChar()
+		if !ok {
+			return strings.ToLower(buf.String()), true
+		}
+		if r <= 32 {
+			break
+		}
+		buf.WriteRune(r)
+	}
+	return strings.ToLower(buf.String()), true
 }
 
 func (f *Forth) runWords(word []string) {
@@ -69,73 +118,223 @@ func (f *Forth) runWords(word []string) {
 	}
 }
 func (f *Forth) runWord(word string) {
+	word = strings.ToLower(word)
+	if *V {
+		log.Printf("RW: %q", word)
+	}
 	// First see if it is an integer.
 	i, err := strconv.ParseInt(word, 10, 64)
 	if err == nil {
 		f.D.Push(int(i))
 		return
 	}
-	// Or else it should be a defined word.
+	// Or else it could be a normal word.
 	fn, ok := f.Words[word]
-	if !ok {
-		log.Panicf("forth: word not found: %q", word)
+	if ok {
+		if fn == nil {
+			Panicf("runWord: word %q is nil", word)
+		}
+		fn()
+		return // normal word normal return.
 	}
-	fn()
+	// Or else it could be an immediate word.
+	imm, ok2 := f.Immediate[word]
+	if !ok2 {
+		Panicf("forth: word not found: %q", word)
+	}
+	word = imm() // Change the word to generated word.
+
+	fn, ok = f.Words[word]
+	if ok {
+		fn()
+		return // generated word normal return.
+	}
+	Panicf("forth: genrated word not found: %q", word)
 }
 func (f *Forth) RunProgram(prog string) {
-	f.input = strings.Fields(prog)
-	for len(f.input) > 0 {
-		f.runWord(f.nextWord())
+	f.Input = prog
+	for {
+		w, ok := f.NextWord()
+		if !ok {
+			break
+		}
+		if *V {
+			log.Printf("RP: %q", w)
+		}
+		f.runWord(w)
 	}
 }
 
 func (f *Forth) compileStepsUntil(enders ...string) (end string, steps []string) {
 	for {
-		w := f.nextWord()
+		w, ok := f.NextWord()
+		if !ok {
+			Panicf("compileStepsUntil got EOF, wanted one of %v", enders)
+		}
 		for _, end = range enders {
 			if w == end {
 				return
 			}
 		}
-		// First look to see if it is an Immediate word.
-		if imm, ok := f.Immediate[w]; ok {
-			// If so, execute the Immediate function, and append the result.
-			steps = append(steps, imm())
-		} else {
-			// Or append the normal word.
-			steps = append(steps, w)
-		}
+
+		func() {
+			defer func() {
+				r := recover()
+				switch t := r.(type) {
+				case CompileThisWordSignal:
+					steps = append(steps, string(t))
+				case nil:
+					return
+				default:
+					panic(r)
+				}
+			}()
+			// First look to see if it is an Immediate word.
+			if imm, ok := f.Immediate[w]; ok {
+				// If so, execute the Immediate function, and append the result.
+				steps = append(steps, imm())
+			} else {
+				// Or append the normal word.
+				steps = append(steps, w)
+			}
+		}()
 	}
 }
 
+func boolToInt(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func DumpInfo(f *Forth) {
+	log.Printf("<<<<<<<<<<<<<<<<")
+	log.Printf("D Stack: %v", f.D)
+	log.Printf("R Stack: %v", f.R)
+	for i, e := range f.Mem {
+		if e != 0 {
+			fmt.Fprintf(os.Stderr, "[%d]=%d ", i, e)
+		}
+	}
+	log.Printf(">>>>>>>>>>>>>>>")
+}
 func (f *Forth) InitWords() {
 	f.Immediate = Idict{
+		"(": func() string {
+			for {
+				r, ok := f.NextChar()
+				if !ok || r == ')' {
+					break
+				}
+			}
+			return "nop"
+		},
+		`\`: func() string {
+			for {
+				r, ok := f.NextChar()
+				if !ok || r == '\n' || r == '\r' {
+					break
+				}
+			}
+			return "nop"
+		},
+		`."`: func() string {
+			var buf strings.Builder
+			for {
+				r, ok := f.NextChar()
+				if !ok {
+					Panicf("`.\"` read EOF, wanted `\"`")
+				}
+				if r == '"' {
+					break
+				}
+				buf.WriteRune(r)
+			}
+			str := buf.String()
+			name := fmt.Sprintf("~emit~str~%d~", f.Here)
+			f.Words[name] = func() {
+				for _, r := range str {
+					f.Emit(byte(r))
+				}
+			}
+			return name
+		},
+		`s"`: func() string {
+			var buf strings.Builder
+			start := f.Here
+			for {
+				r, ok := f.NextChar()
+				if !ok {
+					Panicf("`s\"` read EOF, wanted `\"`")
+				}
+				if r == '"' {
+					break
+				}
+				b := byte(r)
+				buf.WriteByte(b)
+				f.Mem[f.Here] = int(b)
+				f.Here++
+			}
+			f.Mem[f.Here] = 0
+			f.Here++
+			name := fmt.Sprintf("~str~%d~", f.Here)
+			f.Words[name] = func() {
+				f.D.Push(start)
+				f.D.Push(f.Here - start - 1)
+			}
+			return name
+		},
+		"begin": func() string {
+			name := fmt.Sprintf("~if~%d~", f.Here)
+			f.Here++ // we needed a unique name.
+			var first_steps, later_steps []string
+			var has_while bool
+			ender, first_steps := f.compileStepsUntil("while", "repeat")
+			if ender == "while" {
+				_, later_steps = f.compileStepsUntil("repeat")
+				has_while = true
+			}
+			f.Words[name] = func() {
+				for {
+					f.runWords(first_steps)
+					if has_while {
+						cond := f.D.Pop()
+						if cond == 0 {
+							return
+						}
+						f.runWords(later_steps)
+					}
+				}
+			}
+			return name
+		},
 		"if": func() string {
-			name := fmt.Sprintf("if_%d_", f.Here)
+			name := fmt.Sprintf("~if~%d~", f.Here)
 			f.Here++ // we needed a unique name.
 			var then_steps, else_steps []string
 			ender, then_steps := f.compileStepsUntil("else", "then")
 			if ender == "else" {
 				_, else_steps = f.compileStepsUntil("then")
 			}
-			fn := func() {
+			f.Words[name] = func() {
 				if f.D.Pop() != 0 {
 					f.runWords(then_steps)
 				} else {
 					f.runWords(else_steps)
 				}
 			}
-			f.Words[name] = fn
 			return name
 		},
 		"do": func() string {
-			name := fmt.Sprintf("do_%d_", f.Here) // Use Here to create a unique name,
-			f.Here++                              // then waste a slot.
-			_, steps := f.compileStepsUntil("loop")
+			name := fmt.Sprintf("~do~%d~", f.Here) // Use Here to create a unique name,
+			f.Here++                               // then waste a slot.
+			ender, steps := f.compileStepsUntil("loop", "+loop")
+			plus_loop := ender == "+loop"
 			fn := func() {
 				i := f.D.Pop()
 				limit := f.D.Pop()
-				for ; i < limit; i++ {
+				for i < limit {
 					// We don't have a Forth way to access `limit`
 					// or change `i` (yet) but let's put them
 					// both on the R-Stack anyway.
@@ -146,10 +345,32 @@ func (f *Forth) InitWords() {
 					}
 					i = f.R.Pop()
 					limit = f.R.Pop()
+					if plus_loop {
+						i += f.D.Pop()
+					} else {
+						i++
+					}
 				}
 			}
 			f.Words[name] = fn
 			return name
+		},
+		"postpone": func() string {
+			name, ok := f.NextWord()
+			if !ok {
+				Panicf("`variable` got EOF, wanted a word")
+			}
+			fn, ok := f.Immediate[name]
+			if ok {
+				fn()
+				return "nop"
+			}
+			_, ok = f.Words[name]
+			if ok {
+				panic(CompileThisWordSignal(name))
+			}
+			Panicf("`postpone` cannot find word %q", name)
+			return ""
 		},
 	}
 	binop := func(op func(int, int) int) func() {
@@ -160,22 +381,77 @@ func (f *Forth) InitWords() {
 		}
 	}
 	f.Words = Dict{
-		"+": binop(func(x, y int) int { return x + y }),
-		"-": binop(func(x, y int) int { return x - y }),
-		"*": binop(func(x, y int) int { return x * y }),
-		"/": binop(func(x, y int) int { return x / y }),
-		"%": binop(func(x, y int) int { return x % y }),
+		"and":   binop(func(x, y int) int { return x & y }),
+		"or":    binop(func(x, y int) int { return x | y }),
+		"xor":   binop(func(x, y int) int { return x ^ y }),
+		"+":     binop(func(x, y int) int { return x + y }),
+		"-":     binop(func(x, y int) int { return x - y }),
+		"*":     binop(func(x, y int) int { return x * y }),
+		"/":     binop(func(x, y int) int { return x / y }),
+		"%":     binop(func(x, y int) int { return x % y }),
+		"=":     binop(func(x, y int) int { return boolToInt(x == y) }),
+		"==":    binop(func(x, y int) int { return boolToInt(x == y) }),
+		"!=":    binop(func(x, y int) int { return boolToInt(x != y) }),
+		"/=":    binop(func(x, y int) int { return boolToInt(x != y) }),
+		"<>":    binop(func(x, y int) int { return boolToInt(x != y) }),
+		"<":     binop(func(x, y int) int { return boolToInt(x < y) }),
+		"<=":    binop(func(x, y int) int { return boolToInt(x <= y) }),
+		">=":    binop(func(x, y int) int { return boolToInt(x >= y) }),
+		">":     binop(func(x, y int) int { return boolToInt(x > y) }),
+		"nop":   func() {},
+		"cells": func() {},
+		"immediate": func() {
+			name := f.RecentDef
+			fn, ok := f.Words[name]
+			if !ok {
+				Panicf("`immediate` cannot find word %q", name)
+			}
+			delete(f.Words, name)
+			f.Immediate[name] = func() string {
+				fn()
+				return "nop"
+			}
+		},
+		"constant": func() {
+			value := f.D.Pop()
+			name, ok := f.NextWord()
+			if !ok {
+				Panicf("`constant` got EOF, wanted a word")
+			}
+			f.Words[name] = func() {
+				f.D.Push(value)
+			}
+			f.RecentDef = name
+		},
 		"variable": func() {
-			name := f.nextWord()
+			name, ok := f.NextWord()
+			if !ok {
+				Panicf("`variable` got EOF, wanted a word")
+			}
 			i := f.Here
 			f.Here++
-			fn := func() {
+			f.Words[name] = func() {
 				f.D.Push(i)
 			}
-			f.Words[name] = fn
+			f.RecentDef = name
 		},
+		"create": func() {
+			name, ok := f.NextWord()
+			if !ok {
+				Panicf("`variable` got EOF, wanted a word")
+			}
+			i := f.Here
+			f.Words[name] = func() {
+				f.D.Push(i)
+			}
+			f.RecentDef = name
+		},
+		"allot": func() { f.Here += f.D.Pop() },
 		":": func() {
-			name := f.nextWord()
+			name, ok := f.NextWord()
+			if !ok {
+				Panicf("`:` got EOF, wanted a word")
+			}
 			_, steps := f.compileStepsUntil(";")
 			fn := func() {
 				for _, w := range steps {
@@ -183,17 +459,45 @@ func (f *Forth) InitWords() {
 				}
 			}
 			f.Words[name] = fn
+			f.RecentDef = name
 		},
 		"!": func() {
 			i := f.D.Pop()
 			x := f.D.Pop()
 			f.Mem[i] = x
 		},
-		"@": func() { f.D.Push(f.Mem[f.D.Pop()]) },
+		"@":  func() { f.D.Push(f.Mem[f.D.Pop()]) },
+		"c@": func() { f.D.Push(f.Mem[f.D.Pop()]) },
+		"not": func() {
+			f.D.Push(boolToInt(f.D.Pop() == 0))
+		},
+		"0=": func() {
+			f.D.Push(boolToInt(f.D.Pop() == 0))
+		},
+		"1+": func() {
+			f.D.Push(f.D.Pop() + 1)
+		},
+		"1-": func() {
+			f.D.Push(f.D.Pop() - 1)
+		},
+		"negate": func() {
+			f.D.Push(0 - f.D.Pop())
+		},
+		"drop": func() {
+			f.D.Pop()
+		},
 		"dup": func() {
 			x := f.D.Pop()
 			f.D.Push(x)
 			f.D.Push(x)
+		},
+		"2dup": func() {
+			y := f.D.Pop()
+			x := f.D.Pop()
+			f.D.Push(x)
+			f.D.Push(y)
+			f.D.Push(x)
+			f.D.Push(y)
 		},
 		"swap": func() {
 			y := f.D.Pop()
@@ -208,6 +512,13 @@ func (f *Forth) InitWords() {
 				f.Emit(byte(ch))
 			}
 		},
+		"j": func() {
+			i := f.R.Pop()
+			x := f.R.Pop()
+			f.R.Push(x)
+			f.R.Push(i)
+			f.D.Push(x)
+		},
 		"i": func() {
 			x := f.R.Pop()
 			f.R.Push(x)
@@ -221,16 +532,36 @@ func (f *Forth) InitWords() {
 		},
 		"bl": func() { f.D.Push(' ') },
 		"cr": func() { f.Emit('\n') },
-		"?": func() {
-			log.Printf("----------------")
-			log.Printf("D Stack: %v", f.D)
-			log.Printf("R Stack: %v", f.R)
-			for i, e := range f.Mem {
-				if e != 0 {
-					log.Printf("Mem[%d] = %d", i, e)
-				}
+		".s": func() {
+			n := f.D.Pop()
+			ptr := f.D.Pop()
+			for i := 0; i < n; i++ {
+				f.Emit(byte(f.Mem[ptr+i]))
 			}
-			log.Printf("================")
+		},
+		"???": func() {
+			DumpInfo(f)
+		},
+		"words": func() {
+			var words []string
+			for k := range f.Words {
+				words = append(words, k)
+			}
+			sort.Strings(words)
+			for _, w := range words {
+				fmt.Printf("%s ", w)
+			}
+			fmt.Printf("\n")
+			words = nil
+			for k := range f.Immediate {
+				words = append(words, k)
+			}
+			sort.Strings(words)
+			for _, w := range words {
+				fmt.Printf("%s ", w)
+			}
+			fmt.Printf("\n")
 		},
 	}
+	f.Immediate["?do"] = f.Immediate["do"]
 }
--- main.go
+++ main.go
@@ -4,20 +4,85 @@
 package main
 
 import (
-	forth "github.com/strickyak/meekly-go-forth"
+	"flag"
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/chzyer/readline"
+
+	forth "github.com/strickyak/meekly-go-forth"
 )
 
+var FlagI = flag.Bool("i", false, "run interactive shell even if command line scripts are given")
+
 func main() {
-	bb, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		panic(err)
+	flag.Parse()
+
+	f := forth.NewForth(emit)
+
+	for _, filename := range flag.Args() {
+		bb, err := ioutil.ReadFile(filename)
+		if err != nil {
+			log.Fatalf("Cannot read file %q: %v", filename, err)
+		}
+		f.RunProgram(string(bb))
 	}
-	f := forth.NewForth(putchar)
-	f.RunProgram(string(bb))
+
+	if *FlagI || flag.NArg() == 0 {
+
+		home := os.Getenv("HOME")
+		if home == "" {
+			home = "."
+		}
+		rl, err := readline.NewEx(&readline.Config{
+			Prompt:          " ok ",
+			HistoryFile:     filepath.Join(home, ".livy-apl.history"),
+			InterruptPrompt: "*SIGINT*",
+			EOFPrompt:       "*EOF*",
+			// AutoComplete:    completer,
+			// HistorySearchFold:   true,
+			// FuncFilterInputRune: filterInput,
+		})
+		if err != nil {
+			panic(err)
+		}
+		defer rl.Close()
+
+		for {
+			os.Stderr.Write([]byte{'\n'})
+			line, err := rl.Readline()
+			if err == readline.ErrInterrupt {
+				if len(line) == 0 {
+					break
+				} else {
+					continue
+				}
+			} else if err == io.EOF {
+				break
+			}
+			// f.RunProgram(line)
+			TryRunProgram(f, line)
+		}
+	}
 }
 
-func putchar(ch byte) {
-	os.Stdout.Write([]byte{ch})
+func TryRunProgram(f *forth.Forth, line string) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			log.Printf("*** ERROR *** %v", r)
+		}
+	}()
+	f.RunProgram(line)
+}
+
+func emit(ch byte) {
+	//log.Printf("emit byte: %d", ch)
+	_, err := os.Stdout.Write([]byte{ch})
+	if err != nil {
+		log.Fatalf("cannot emit: %v", err)
+	}
 }
